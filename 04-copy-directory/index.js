2fa94453ec929129cb5be403123d5f2f7c6ea46b
DelfiaHope <valeruanovna94@mail.ru>
2022-05-15 23:28:07 +0300

    feat: create copy file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+  
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) { 
+      console.error(err);
+    }    
+  });
 
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+if(err) {console.log('error');}
+else{
+    files.forEach((file) => {
+        fs.writeFile(
+            path.join(__dirname, 'files-copy', `${file}`), '',
+            (err) => { if(err) console.log('fac0');       
+            }
+        );
+        fs.copyFile(
+            path.join(__dirname, 'files', `${file}`), 
+            path.join(__dirname, 'files-copy', `${file}`), err => {if(err)console.log('fac1') })
+    })  
+}
+})
