3cfeaf30f448492cc9ad50e1decdf2c32913de4c
DelfiaHope <valeruanovna94@mail.ru>
2022-05-25 00:30:55 +0300

    fix: copy change files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,30 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 
-fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
-    if (err) console.log(err);        
-});
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: false}, (err) => {
+     
+      
+    fs.readdir(path.join(__dirname, 'files'), (err, file) => {
+        if(err) console.log(err);          
+        
+        for(let i = 0; i < file.length; i++) {
+            fs.copyFile(
+                path.join(__dirname, 'files', `${file[i]}`),
+                path.join(__dirname, 'files-copy', `${file[i]}`), (err) => {
+                  if(err) console.log(err); 
+            });
+            
+        }
+      });  
 
-fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
-  if(err) console.log(err);
-    files.forEach((file) => {
-      fs.unlink(path.join(__dirname, 'files-copy', `${file}`), (err) => {
-        if (err) console.log(err);      
-      });
-    })
-
-});
-
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-if(err) console.log('error');
-    files.forEach((file) => {
-        fs.writeFile(
-            path.join(__dirname, 'files-copy', `${file}`), '',
-            (err) => { if(err) console.log('fac0', err);       
-            }
-        );
-        fs.copyFile(
-            path.join(__dirname, 'files', `${file}`), 
-            path.join(__dirname, 'files-copy', `${file}`), err => {if(err) console.log('fac1', err) })
-    }) 
+  })
+  
 })
